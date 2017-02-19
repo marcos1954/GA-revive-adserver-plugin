55f8c9b807ede7651a1bef33f6d4afe8893b7a4a
marcos1954 <mark@gayguidevallarta.com>
2017-02-19 07:58:40 -0600

    convert to distribution name from developer name version

---
+++ plugins/deliveryAdRender/GoogleAnalytics/GoogleAnalytics.delivery.php
@@ -0,0 +1,64 @@
+<?php
+
+MAX_Dal_Delivery_Include();
+
+function Plugin_deliveryAdRender_GoogleAnalytics_GoogleAnalytics_Delivery_postAdRender(&$code, $aBanner) {
+	$base = $GLOBALS['_MAX']['CONF']['GoogleAnalytics'];
+	
+	// if no GA id has been set we can't do anything
+	// also if no track options are set in plugin settings, do nothing
+	//
+	if( ($base['analyticid'] == '') || (!$base['trackClick'] && !$base['trackDisplay']) )
+		return;
+
+	$url = $aBanner['url'];		// click destination url for this banner
+	$id = $base['analyticid'];	// Google Analytics ID from plugin settings
+
+	// text fields used as Category, Event Action, and Event Label for events sent to GA
+	// these are configured in plugin settings
+	//
+	$categoryImpression = addslashes($base['impressionCatName']);
+	$actionImpression = addslashes($base['impressionAction']);
+	$categoryClick = addslashes($base['clickCatName']);
+	$actionClick = addslashes($base['clickAction']);
+
+	$label = addslashes($aBanner['name']).( ($base['bannerSize']) ? (' '.$aBanner['width']).'x'.($aBanner['height']) : '');
+
+	// build script to be inserted in postAdRender.  Loads analytics.js and initializes tracker if needed.
+	// if plugin is configured to track impressions, sends impression GA event after initialization. 
+	//
+	$aGcode = "
+		<script>
+		if (typeof ga == 'undefined') {
+			(function(i, s, o, g, r, a, m) {i['markAnalyticsObject'] = r;i[r] = i[r] || function() {(i[r].q = i[r].q || []).push(arguments)}, i[r].l = 1 * new Date(); a = s.createElement(o),m = s.getElementsByTagName(o)[0];a.async = 1;a.src = g;m.parentNode.insertBefore(a, m)})   (window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+			ga('create', '".$id."', 'auto');
+		}
+		";
+	if ($base['trackDisplay']) {
+		$aGcode .= "ga('send', 'event', '$categoryImpression', '$actionImpression', '$label', {'transport': 'beacon', nonInteraction: true});";
+	}
+	$aGcode .= "</script>" ;
+
+	// splice onclick javascript for GA event into html anchor element already in $code.
+	// the GA event send callback navigates to new page.  using the callback assures
+	// that the event will be sent before navigating away from page
+	//
+	if ($base['trackClick']) {
+		$target = get_string_between($code, 'target=\'', '\'');
+		$search = 'target=';
+		$replace = "onclick=\" ga('send', 'event', '$categoryClick', '$actionClick', '$label', {'transport':'beacon','hitCallback':function(){window.open('$url',target='$target');}}); return false; \"   target=";
+		
+		$code = str_replace($search, $replace, $code);
+	}
+
+	$code = $code.$aGcode; 
+}
+
+function get_string_between($string, $start, $end) {
+    $string = ' ' . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0) return '';
+    $ini += strlen($start);
+    $len = strpos($string, $end, $ini) - $ini;
+    return substr($string, $ini, $len);
+}
